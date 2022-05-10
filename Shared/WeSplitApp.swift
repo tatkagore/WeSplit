e4771a2d575ca1c6522d93df7b63ee3aa5b66016
Tatiana Simmer <tatkagore@gmail.com>
2022-05-10 10:46:39 +0200

    Initial Commit

---
+++ Shared/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  Shared
+//
+//  Created by Tatiana Simmer on 10/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
